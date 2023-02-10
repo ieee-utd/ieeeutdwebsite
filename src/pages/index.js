4d0e45297c3367e7eb8cd57eac264df63c59fc60
jduhking <112415714+jduhking@users.noreply.github.com>
2023-02-10 00:22:01 -0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>This is the home page</p>
+        <p>James was here</p>
       </main>
     </>
   );
