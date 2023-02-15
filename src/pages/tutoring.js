4b7b58d392273fa2ffc1522f68b5eb3fa5b54043
Steven <steveisthenguyener@gmail.com>
2023-02-14 21:17:45 -0600

    ready to merge

--- src/pages/tutoring.js
+++ src/pages/tutoring.js
@@ -2,130 +2,8 @@ import Head from "next/head";
 import styles from "../styles/Tutoring.module.css";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import listPlugin from "@fullcalendar/list";
+import { tutoringSchedule } from "../components/tutoringschedule.js";
 
-const events = [
-  {
-    title: 'Ben - 3320',
-    start: '2023-02-06T10:00:00',
-    end: '2023-02-06T12:00:00'
-  },
-  {
-    title: 'Brent - 3301',
-    start: '2023-02-06T10:00:00',
-    end: '2023-02-06T11:00:00'
-  },
-  {
-    title: 'Mandy - 1202', 
-    start: '2023-02-06T10:00:00', 
-    end: '2023-02-06T11:00:00'
-  },
-  {
-    title: 'Osayamen - 3302',
-    start: '2023-02-06T12:00:00',
-    end: '2023-02-06T13:00:00'
-  },
-  {
-    title: 'Steven - 2310',
-    start: '2023-02-06T12:00:00',
-    end: '2023-02-06T13:00:00'
-  },
-  {
-    title: 'Pranav - 3320',
-    start: '2023-02-06T13:00:00',
-    end: '2023-02-06T14:00:00'
-  },
-  {
-    title: 'Francisco - 1202',
-    start: '2023-02-07T10:00:00',
-    end: '2023-02-07T12:00:00'
-  },
-  {
-    title: 'Isaac - 2310',
-    start: '2023-02-07T10:00:00',
-    end: '2023-02-07T11:00:00'
-  },
-  {
-    title: 'Brent - 3301',
-    start: '2023-02-07T11:30:00',
-    end: '2023-02-07T12:30:00'
-  },
-  {
-    title: 'Bryce - 3320',
-    start: '2023-02-07T11:30:00',
-    end: '2023-02-07T12:30:00'
-  },
-  {
-    title: 'Nikhil - CS1325',
-    start: '2023-02-07T13:00:00',
-    end: '2023-02-07T15:00:00'
-  },
-  {
-    title: 'Gordon - 3301',
-    start: '2023-02-07T13:00:00',
-    end: '2023-02-07T14:00:00'
-  },
-  {
-    title: 'Megan - 2310',
-    start: '2023-02-07T15:00:00',
-    end: '2023-02-07T16:00:00'
-  },
-  {
-    title: 'Osayamen - 3302',
-    start: '2023-02-08T12:00:00',
-    end: '2023-02-08T13:00:00'
-  },
-  {
-    title: 'Steven - 2310',
-    start: '2023-02-08T12:00:00',
-    end: '2023-02-08T13:00:00'
-  },
-  {
-    title: 'Sebastian - 4301',
-    start: '2023-02-08T13:00:00',
-    end: '2023-02-08T15:00:00'
-  },
-  {
-    title: 'Pranav - 3320',
-    start: '2023-02-08T13:00:00',
-    end: '2023-02-08T14:00:00'
-  },
-  {
-    title: 'Isaac - 2310',
-    start: '2023-02-09T10:00:00',
-    end: '2023-02-09T11:00:00'
-  },
-  {
-    title: 'Benjamin - 3201',
-    start: '2023-02-09T11:00:00',
-    end: '2023-02-09T13:00:00'
-  },
-  {
-    title: 'Bryce - 3320',
-    start: '2023-02-09T11:30:00',
-    end: '2023-02-09T12:30:00'
-  },
-  {
-    title: 'Joshua - 3301',
-    start: '2023-02-09T13:00:00',
-    end: '2023-02-09T15:00:00'
-  },
-  {
-    title: 'Gordon - 3301',
-    start: '2023-02-09T13:00:00',
-    end: '2023-02-09T14:00:00'
-  },
-  {
-    title: 'Megan - 2310',
-    start: '2023-02-09T15:00:00',
-    end: '2023-02-09T16:00:00'
-  },
-  {
-    title: 'Mandy - 1202',
-    start: '2023-02-10T11:30:00',
-    end: '2023-02-10T12:30:00'
-  }
-]
 export default function Tutoring() {
   return (
     <>
@@ -143,6 +21,12 @@ export default function Tutoring() {
               <FullCalendar className={styles.calendar}
                 plugins={[timeGridPlugin]}  
                 initialView='timeGridWeek'
+                validRange = {{
+                  start: '2022-02-06',
+                  end: '2023-02-5'
+                }}
+                initialDate={'2023-02-06'}
+                
                 slotDuration="00:15:00"
                 slotMinTime="10:00"
                 slotMaxTime="16:30"
@@ -150,7 +34,7 @@ export default function Tutoring() {
                 weekends={false}
                 // dayHeaderFormat={{weekday: 'long'}}
                 allDaySlot={false}
-                events={events}
+                events={tutoringSchedule}
                 headerToolbar={{
                   left: '',
                   center: '',
