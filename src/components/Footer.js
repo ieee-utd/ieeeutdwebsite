1f1b0785236416cef1948a0045669cb1268946c0
jduhking <112415714+jduhking@users.noreply.github.com>
2023-02-16 14:41:17 -0600

    Fixed the navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
 import styles from "../styles/Footer.module.css";
 
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer>
-      <div className={styles.footer}>Copyright</div>
+    <footer className=" bg-ieeeorange h-1/2 w-full  flex md:flex-row flex-col justify-around items-start p-20">
+      <div className="text-white p-5">
+        <p>© Copyright of IEEE at UTD</p>
+      </div>
+      <div className="p-5">
+        <ul>
+          <div className="align-items: center">Our Socials</div>
+          <div className="group flex gap-6 pb-5">
+            <FaInstagram className="transition ease-in-out delay-150 text-white text-6xl cursor-pointer hover:fill-white group-hover:text-ieeeorangedim" />
+            <FaFacebook className="transition ease-in-out delay-150 text-white  text-6xl cursor-pointer hover:fill-white group-hover:text-ieeeorangedim " />
+            <FaLinkedin className="transition ease-in-out delay-150 text-white  text-6xl cursor-pointer hover:fill-white group-hover:text-ieeeorangedim" />
+            <FaYoutube className="transition ease-in-out delay-150 text-white text-6xl cursor-pointer hover:fill-white group-hover:text-ieeeorangedim" />
+          </div>
+        </ul>
+      </div>
     </footer>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import styles from "@/styles/Navigation.module.css";
 import Image from "next/image";
-import NavOption from "./NavOption";
+import Link from "next/Link";
+
 import logo from "../assets/ieeeutdlogo.png";
 import HamburgerMenu from "./HamburgerMenu";
 
@@ -11,11 +12,38 @@ export default function Navbar() {
         <Image className={styles.logo} src={logo} alt="IEEELOGO" />
         <div className={styles.navbar__title}>IEEE UTD</div>
       </div>
-      <NavOption menuOption="Home" />
-      <NavOption menuOption="Tutoring" page="tutoring" />
-      <NavOption menuOption="Events" page="events" />
-      <NavOption menuOption="Committees" page="committees" />
-      <NavOption menuOption="Forge" page="forge" />
+      <div className="group hidden gap-6 pb-5 mt-6 md:flex mr-10">
+        <Link
+          href={"/"}
+          className="transition ease-in-out delay-150 text-white cursor-pointer text-2xl group-hover:text-fadedblue"
+        >
+          <p className={styles.navbar__hover__underline}>Home</p>
+        </Link>
+        <Link
+          className="transition ease-in-out delay-150 text-white cursor-pointer text-2xl group-hover:text-fadedblue"
+          href={"/events"}
+        >
+          <p className={styles.navbar__hover__underline}>Events</p>
+        </Link>
+        <Link
+          className="transition ease-in-out delay-150 text-white cursor-pointer text-2xl group-hover:text-fadedblue"
+          href={"/tutoring"}
+        >
+          <p className={styles.navbar__hover__underline}>Tutoring</p>
+        </Link>
+        <Link
+          className="transition ease-in-out delay-150 text-white cursor-pointer text-2xl group-hover:text-fadedblue"
+          href={"/committees"}
+        >
+          <p className={styles.navbar__hover__underline}>Committees</p>
+        </Link>
+        <Link
+          className="transition ease-in-out delay-150 text-white cursor-pointer text-2xl group-hover:text-fadedblue"
+          href={"/forge"}
+        >
+          <p className={styles.navbar__hover__underline}>Forge</p>
+        </Link>
+      </div>
       <div className={styles.hamburger_container}>
         <HamburgerMenu />
       </div>
