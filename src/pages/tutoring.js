b7dc028bed31da836cedcb371fdbbbb03b87430f
Steven <steveisthenguyener@gmail.com>
2023-02-18 20:09:03 -0600

    disable click event

--- src/pages/tutoring.js
+++ src/pages/tutoring.js
@@ -38,6 +38,10 @@ export default function Tutoring() {
                   center: '',
                   right: '',
                 }}
+                eventClick= {function (event) {
+                  // Prevent redirect to Google Calendar
+                  event.jsEvent.preventDefault();
+              }}
                 />
               </div>
           </div>
