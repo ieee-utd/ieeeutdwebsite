87a51b4ffb1bd8fda1addca4938f79277bf7d874
Pelps12 <oluwapelps@gmail.com>
2023-03-03 20:13:59 -0600

    Add workshops

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,71 @@
-import styles from "@/styles/Navigation.module.css";
-import Image from "next/image";
-import Link from "next/link";
-
-import logo from "../assets/ieeeutdlogo.png";
-import HamburgerMenu from "./HamburgerMenu";
+import styles from '@/styles/Navigation.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import logo from '../assets/ieeeutdlogo.png';
+import HamburgerMenu from './HamburgerMenu';
 
 export default function Navbar() {
-  return (
-    <header className={styles.navbar}>
-      <div className={styles.first__container}>
-        <Link href={"/"} className={styles.title__logo}>
-          <Image className={styles.logo} src={logo} alt="IEEELOGO" />
+	const router = useRouter();
+	React.useEffect(() => {
+		console.log(router.pathname);
+	}, []);
+	return (
+		<header className={styles.navbar}>
+			<div className={styles.first__container}>
+				<Link href={'/'} className={styles.title__logo}>
+					<Image className={styles.logo} src={logo} alt="IEEELOGO" />
 
-          <div className={styles.navbar__title}>IEEE UTD</div>
-        </Link>
-      </div>
+					<div className={styles.navbar__title}>IEEE UTD</div>
+				</Link>
+			</div>
 
-      <div className="tw-group tw-hidden tw-gap-6 tw-pb-5 tw-mt-6 md:tw-flex tw-mr-10">
-        <Link
-          href={"/"}
-          className="tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue"
-        >
-          <p className={styles.navbar__hover__underline}>Home</p>
-        </Link>
-        <Link
-          className="tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue"
-          href={"/events"}
-        >
-          <p className={styles.navbar__hover__underline}>Events</p>
-        </Link>
-        <Link
-          className="tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue"
-          href={"/tutoring"}
-        >
-          <p className={styles.navbar__hover__underline}>Tutoring</p>
-        </Link>
-        <Link
-          className="tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue"
-          href={"/committees"}
-        >
-          <p className={styles.navbar__hover__underline}>Committees</p>
-        </Link>
-        <Link
-          className="tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue"
-          href={"/forge"}
-        >
-          <p className={styles.navbar__hover__underline}>Forge</p>
-        </Link>
-      </div>
-      <div className={styles.hamburger_container}>
-        <HamburgerMenu />
-      </div>
-    </header>
-  );
+			<div className="tw-group tw-hidden tw-gap-6 tw-pb-5 tw-mt-6 md:tw-flex tw-mr-10">
+				<Link
+					href={'/'}
+					className={`tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue ${
+						router.pathname == '/' ? 'tw-text-ieeeorangedim  ' : ''
+					}`}
+				>
+					<p className={styles.navbar__hover__underline}>Home</p>
+				</Link>
+				<Link
+					className={`tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue ${
+						router.pathname == '/events' ? 'tw-text-ieeeorangedim ' : ''
+					}`}
+					href={'/events'}
+				>
+					<p className={styles.navbar__hover__underline}>Events</p>
+				</Link>
+				<Link
+					className={`tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue ${
+						router.pathname == '/tutoring' ? 'tw-text-ieeeorangedim' : ''
+					}`}
+					href={'/tutoring'}
+				>
+					<p className={styles.navbar__hover__underline}>Tutoring</p>
+				</Link>
+				<Link
+					className={`tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue ${
+						router.pathname == '/committees' ? 'tw-text-ieeeorangedim ' : ''
+					}`}
+					href={'/committees'}
+				>
+					<p className={styles.navbar__hover__underline}>Committees</p>
+				</Link>
+				<Link
+					className={`tw-transition tw-ease-in-out tw-delay-150 tw-text-white tw-cursor-pointer tw-text-2xl group-hover:tw-text-fadedblue ${
+						router.pathname == '/forge' ? 'tw-text-ieeeorangedim' : ''
+					}`}
+					href={'/forge'}
+				>
+					<p className={styles.navbar__hover__underline}>Forge</p>
+				</Link>
+			</div>
+			<div className={styles.hamburger_container}>
+				<HamburgerMenu />
+			</div>
+		</header>
+	);
 }
--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -13,43 +13,47 @@ const EventsPage = () => {
 	React.useEffect(() => {
 		width > 768 ? 'listPlugin' : 'dayGridPlugin';
 	}, [width]);
-	React.useEffect(() => {
-		// declare the data fetching function
-		const fetchData = async () => {
-			const response = await fetch('/api/google/redirect');
-			if (response.ok) {
-				const result = await response.json();
-				console.log(result);
-				setEvents(result.message.sort((a, b) => a.start.dateTime - b.start.dateTime));
-			}
-		};
-
-		// call the function
-		fetchData()
-			// make sure to catch any error
-			.catch(console.error);
-	}, []);
 
 	return (
 		<>
-			{
-				<FullCalendar
-					plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
-					ref={calendarRef}
-					googleCalendarApiKey="AIzaSyAfPLw0W2oEPGS_GaXsJhCpaZw0zTXAxOI"
-					initialView={width < 768 ? 'listMonth' : 'dayGridMonth'}
-					windowResize={function (view) {
-						if (window.innerWidth >= 768) {
-							calendarRef.current.getApi().changeView('dayGridMonth');
-						} else {
-							calendarRef.current.getApi().changeView('listMonth');
-						}
-					}}
-					events={{
-						googleCalendarId: 'e9nb18a17qf698h72g2tr4ecao@group.calendar.google.com'
-					}}
-				/>
-			}
+			<div className="tw-p-6 mx-auto tw-gap-7 tw-flex tw-flex-col tw-items-center">
+				<div className="tw-text-center tw-text-5xl tw-underline tw-font-bold m-6">Events</div>
+
+				<div className="tw-my-12 tw-font-semibold tw-text-2xl tw-max-w-lg">
+					Over the course of the school year, we provide{' '}
+					<span className="tw-text-[#CF165F]">workshops</span>,{' '}
+					<span className="tw-text-[#3277D5]">general</span> , and social events.
+				</div>
+				<div className="tw-w-full lg:tw-w-2/3">
+					<FullCalendar
+						plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
+						ref={calendarRef}
+						headerToolbar={{
+							right: 'listMonth,dayGridMonth,prev,next'
+						}}
+						googleCalendarApiKey="AIzaSyAfPLw0W2oEPGS_GaXsJhCpaZw0zTXAxOI"
+						selectable={false}
+						initialView={'listMonth'}
+						windowResize={function (view) {
+							if (window.innerWidth >= 768) {
+								calendarRef.current.getApi().changeView('dayGridMonth');
+							} else {
+								calendarRef.current.getApi().changeView('listMonth');
+							}
+						}}
+						eventSources={[
+							{
+								googleCalendarId: 'e9nb18a17qf698h72g2tr4ecao@group.calendar.google.com',
+								color: '#3277D5'
+							},
+							{
+								googleCalendarId: 'kren4e2ph3dccrj4og6v780rsc@group.calendar.google.com',
+								color: '#CF165F'
+							}
+						]}
+					/>
+				</div>
+			</div>
 		</>
 	);
 };
