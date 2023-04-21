1af22241406a4debd6ddcb86bba435f54da34670
jduhking <112415714+jduhking@users.noreply.github.com>
2023-04-21 16:11:05 -0500

    image optimization

--- src/components/Developers.js
+++ src/components/Developers.js
@@ -1,41 +1,41 @@
-import styles from "@/styles/Committees.module.css";
-import Member from "@/components/Member";
-import committeeMembers from "./committeeMembers";
+import styles from '@/styles/Committees.module.css';
+import Member from '@/components/Member';
+import committeeMembers from './CommitteeMembers';
 
 export default function Developers() {
-  return (
-    <div className={styles.image__container}>
-      <Member
-        name="James Odebiyi"
-        title="Lead Web Developer"
-        linkedin="https://www.linkedin.com/in/james-odebiyi-a87049214/"
-        email="jxo190003@utdallas.edu"
-      />
-      <Member
-        name="Oluwapelumi Adegoke"
-        title="Assistant Web Developer"
-        linkedin="https://www.linkedin.com/in/oluwapelumi-adegoke-9634561b6/"
-        email="Oluwapelumi.Adegoke@UTDallas.edu"
-      />
-      <Member
-        name="Steven Nguyen"
-        title="Assistant Web Developer"
-        linkedin="https://www.linkedin.com/in/stevenknguyen/"
-        email="Steven.Nguyen7@UTDallas.edu"
-      />
-      <Member
-        name="Rita Kaushik"
-        title="Assistant Web Developer"
-        linkedin="https://www.linkedin.com/in/ritakaushik/"
-        email="rxk200044@utdallas.edu"
-      />
-      <Member
-        name="Arhum Khan"
-        image={committeeMembers[14]["src"]}
-        title="Assistant Web Developer"
-        linkedin="https://www.linkedin.com/in/arhum-khan47/"
-        email="Arhum.Khan@UTDallas.edu"
-      />
-    </div>
-  );
+	return (
+		<div className={styles.image__container}>
+			<Member
+				name="James Odebiyi"
+				title="Lead Web Developer"
+				linkedin="https://www.linkedin.com/in/james-odebiyi-a87049214/"
+				email="jxo190003@utdallas.edu"
+			/>
+			<Member
+				name="Oluwapelumi Adegoke"
+				title="Assistant Web Developer"
+				linkedin="https://www.linkedin.com/in/oluwapelumi-adegoke-9634561b6/"
+				email="Oluwapelumi.Adegoke@UTDallas.edu"
+			/>
+			<Member
+				name="Steven Nguyen"
+				title="Assistant Web Developer"
+				linkedin="https://www.linkedin.com/in/stevenknguyen/"
+				email="Steven.Nguyen7@UTDallas.edu"
+			/>
+			<Member
+				name="Rita Kaushik"
+				title="Assistant Web Developer"
+				linkedin="https://www.linkedin.com/in/ritakaushik/"
+				email="rxk200044@utdallas.edu"
+			/>
+			<Member
+				name="Arhum Khan"
+				image={committeeMembers[14]['src']}
+				title="Assistant Web Developer"
+				linkedin="https://www.linkedin.com/in/arhum-khan47/"
+				email="Arhum.Khan@UTDallas.edu"
+			/>
+		</div>
+	);
 }
