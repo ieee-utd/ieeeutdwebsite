45805f83d9ef709cf7508c5405bb123749171546
jduhking <112415714+jduhking@users.noreply.github.com>
2023-04-21 00:03:58 -0500

    mobile responsive home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
