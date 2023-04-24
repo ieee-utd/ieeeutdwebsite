bd453fea8e8bdca067ad0da44b142c61c4dac09a
jduhking <112415714+jduhking@users.noreply.github.com>
2023-04-24 01:14:48 -0500

    added group photo add the top

--- src/pages/committees.js
+++ src/pages/committees.js
@@ -5,39 +5,48 @@ import Tutoring from '@/components/Tutoring';
 import Forge from '@/components/Forge';
 import Developers from '@/components/Developers';
 import styles from '@/styles/Committees.module.css';
+import Image from 'next/image';
+import Officers from '@/assets/IEEE/Officers.png';
 
 export default function Committees() {
-
-			
 	return (
-		<div className={styles.mainContainer}>
-			<div className={styles.title}>Executives</div>
-			<Executives />
-			<div className={styles.title}>Events Committee</div>
-			<div className={styles.description}>
-				Plan and market upcoming events, moderate the social media accounts, and network with
-				companies
+		<div>
+			<div className={styles.group__container}>
+				<div className={styles.overlay}>
+					<div className={styles.overlay__title}>COMMITTEES</div>
+				</div>
+				<Image className={styles.group__img} src={Officers} alt="ieee-officer-group" />
 			</div>
-			<EventsCommittee />
-			<div className={styles.title}>Tech Committee</div>
-			<div className={styles.description}>
-				Organize and run the IEEE Microcontroller and Soldering workshops
+			<div className={styles.mainContainer}></div>
+			<div>
+				<div className={styles.title}>Executives</div>
+				<Executives />
+				<div className={styles.title}>Events Committee</div>
+				<div className={styles.description}>
+					Plan and market upcoming events, moderate the social media accounts, and network with
+					companies
+				</div>
+				<EventsCommittee />
+				<div className={styles.title}>Tech Committee</div>
+				<div className={styles.description}>
+					Organize and run the IEEE Microcontroller and Soldering workshops
+				</div>
+				<TechCommittee />
+				<div className={styles.title}>Tutoring Committee</div>
+				<div className={styles.description}>
+					Student-led tutoring for many engineering courses in person and online
+				</div>
+				<Tutoring />
+				<div className={styles.title}>Forge</div>
+				<div className={styles.description}>
+					Engineer projects for solutions to real-world problems faced by the UT Dallas community,
+					designed, built, tested, and used by UTD students, faculty and staff
+				</div>
+				<Forge />
+				<div className={styles.title}>Web Developers</div>
+				<div className={styles.description}>Developed the website</div>
+				<Developers />
 			</div>
-			<TechCommittee  />
-			<div className={styles.title}>Tutoring Committee</div>
-			<div className={styles.description}>
-				Student-led tutoring for many engineering courses in person and online
-			</div>
-			<Tutoring />
-			<div className={styles.title}>Forge</div>
-			<div className={styles.description}>
-				Engineer projects for solutions to real-world problems faced by the UT Dallas community,
-				designed, built, tested, and used by UTD students, faculty and staff
-			</div>
-			<Forge />
-			<div className={styles.title}>Web Developers</div>
-			<div className={styles.description}>Developed the website</div>
-			<Developers />
 		</div>
 	);
 }
--- src/pages/forge.js
+++ src/pages/forge.js
@@ -1,53 +1,68 @@
-
-import styles from "@/styles/Forge.module.css";
+import styles from '@/styles/Forge.module.css';
 
 export default function Forge() {
-  return (
-  <div className={styles.mainContainer}>
-      <div className={styles.main}>
-        <h1>What is Forge?</h1>
-        <p>Forge is a project-based committee that aims to engineer solutions to real-world problems faced by the UT Dallas community. 
-          These projects are designed, built, tested, and used by UTD students, faculty and staff.</p>
-      </div>
-      <div className={styles.projects}>
-          <h1>Projects</h1>
-          <div className={styles.title_description}>
-          <p>Forge currently offers the following projects</p>
-          </div>
-          <div className={styles.spacer}>
-            <ul>
-              <li>
-                <h1>8-Bit Processor</h1>
-                <p >Build an 8-bit CPU with a team, tasked with designing and implementing an instruction set while incorporating 
-                  control, memory, a data path, an ALU, and an interface.</p>
-                  <div className={styles.button}>
-                  <a>Learn More</a>
-                </div>
-              </li>
-              <li>
-                <h1>Motion-Tracking Camera</h1>
-                <p>Design a camera that tracks an object&apos;s movement (such as a face) using computer vision software and hardware 
-                  (servos and a micro-controller) to follow it in real-time.</p>
-                <div className={styles.button}>
-                <a>Learn More</a>
-                </div>
-                </li>
-              <li><h1>Dynamic Poster</h1>
-              <p>Design and develop a dynamic picture frame using a micro-controller 
-                that is able to display animated photos and videos</p>
-                <div className={styles.button}>
-                <a>Learn More</a>
-                </div>
-                </li>
-              <li><h1>Voice-Controlled Car</h1>
-              <p>Build a car that can be controlled by voice commands, using machine learning models 
-                and micro-controllers to accurately interpret and respond to user input.</p>
-                <div className={styles.button}>
-                <a>Learn More</a>
-                </div></li>
-            </ul>
-          </div>
-      </div>
-      
-    </div>);
+	return (
+		<div className={styles.mainContainer}>
+			<div className={styles.main}>
+				<h1>What is Forge?</h1>
+				<p>
+					Forge is a project-based committee that aims to engineer solutions to real-world problems
+					faced by the UT Dallas community. These projects are designed, built, tested, and used by
+					UTD students, faculty and staff.
+				</p>
+			</div>
+			<div className={styles.projects}>
+				<h1>Projects</h1>
+				<div className={styles.title_description}>
+					<p>Forge currently offers the following projects</p>
+				</div>
+				<div className={styles.spacer}>
+					<ul>
+						<li>
+							<h1>8-Bit Processor</h1>
+							<p>
+								Build an 8-bit CPU with a team, tasked with designing and implementing an
+								instruction set while incorporating control, memory, a data path, an ALU, and an
+								interface.
+							</p>
+							<div className={styles.button}>
+								<a>Learn More</a>
+							</div>
+						</li>
+						<li>
+							<h1>Motion-Tracking Camera</h1>
+							<p>
+								Design a camera that tracks an object&apos;s movement (such as a face) using
+								computer vision software and hardware (servos and a micro-controller) to follow it
+								in real-time.
+							</p>
+							<div className={styles.button}>
+								<a>Learn More</a>
+							</div>
+						</li>
+						<li>
+							<h1>Dynamic Poster</h1>
+							<p>
+								Design and develop a dynamic picture frame using a micro-controller that is able to
+								display animated photos and videos
+							</p>
+							<div className={styles.button}>
+								<a>Learn More</a>
+							</div>
+						</li>
+						<li>
+							<h1>Voice-Controlled Car</h1>
+							<p>
+								Build a car that can be controlled by voice commands, using machine learning models
+								and micro-controllers to accurately interpret and respond to user input.
+							</p>
+							<div className={styles.button}>
+								<a>Learn More</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
