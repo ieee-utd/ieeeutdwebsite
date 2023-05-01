a5cf4800132b06f1581efba911b56ba657034a80
jduhking <jamesbodebiyi@gmail.com>
2023-04-30 19:19:05 -0500

    fixed kevin

--- src/components/committeeMembers.js
+++ src/components/committeeMembers.js
@@ -88,7 +88,7 @@ const committeeMembers = [
   {
     id: 17,
     name: "Kevin Debord",
-    src: require("../assets/IEEE/Kevin_Debord.jpg"),
+    src: require("../assets/IEEE/Kevin_Debord.JPG"),
   },
  
   {
