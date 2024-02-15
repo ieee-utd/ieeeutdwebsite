567daa32400401776b8458c02eb083cf770392e8
Steven <steveisthenguyener@gmail.com>
2024-02-15 17:22:36 -0600

    fade in effect

--- src/components/committeeMembers.js
+++ src/components/committeeMembers.js
@@ -2,35 +2,35 @@ const committeeMembers = [
     {
         name: "Allison Pham",
         title: "President",
-        image: require("../assets/IEEE/Allison_Pham.png"),
+        image: require("../assets/IEEE/Allison.jpeg"),
         linkedin: "https://www.linkedin.com/in/allison-nhi-pham/",
         email: "anp200002@utdallas.edu",
     },
     {
         name: "Jacob Martin",
         title: "Vice President",
-        image: "",
+        image: require('../assets/IEEE/Jacob.jpeg'),
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "jrm190007@utdallas.edu",
     },
     {
         name: "Gaby Tobias",
         title: "Treasurer",
-        image: "",
+        image: require("../assets/IEEE/Gaby.jpeg"),
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "gst200000@utdallas.edu",
     },
     {
         name: "Alex Monroy",
         title: "Secretary",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Alex.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "aas170530@utdallas.edu",
     },
     {
         name: "Kevin Debord",
         title: "Advisor",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Kevin.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "kjd200004@utdallas.edu",
     },
@@ -43,28 +43,28 @@ const committeeMembers = [
     {
         name: "Nethra",
         title: "Director of Tutoring",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Nethra.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "nxs200035@utdallas.edu",
     },
     {
         name: "Laura Pinto",
         title: "Director of Operations",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Laura.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "lsp210000@utdallas.edu",
     },
     {
         name: "James Odebiyi",
         title: "Web Master",
-        image: require("../assets/IEEE/James_Odebiyi.png"),
+        image: require("../assets/IEEE/James.jpeg"),
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "jxo190003@utdallas.edu",
     },
     {
         name: "Mason",
         title: "Director of Business",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Mason.jpeg"), 
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "msr200000@utdallas.edu",
     },
@@ -78,35 +78,35 @@ const committeeMembers = [
     {
         name: "Jane Bae",
         title: "RF Director",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Jane.jpeg"),
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "jxb190001@utdallas.edu"
     },
     {
         name: "Muneeb",
         title: "RF Project Supervisor",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Muneeb.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "mss200007@utdallas.edu",
     },
     {
         name: "Zaid",
         title: "PES Project Supervisor",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Zaid.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "mxz190024@utdallas.edu",
     },
     {
         name: "Manuel Contreras",
         title: "PES Director",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Manuel.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "mad210014@utdallas.edu",
     },
     {
         name: "Ian Kintz",
         title: "Director of Robotics",
-        image: "", // Please provide the image if available
+        image: require("../assets/IEEE/Ian.jpeg"), // Please provide the image if available
         linkedin: "", // Please provide the LinkedIn profile if available
         email: "ijk180001@utdallas.edu",
     },
--- src/pages/officers.js
+++ src/pages/officers.js
@@ -1,34 +1,52 @@
-// import Executives from '@/components/Executives';
 import EventsCommittee from '@/components/EventsCommittee';
-// import TechCommittee from '@/components/TechCommittee';
-// import Tutoring from '@/components/Tutoring';
-// import Pes from '@/components/Pes'
-// import Developers from '@/components/Developers';
 import styles from '@/styles/Committees.module.css';
 import Image from 'next/image';
 import Officers from '@/assets/IEEE/Officers.jpeg';
 import officerData from '@/components/committeeMembers';
 import Member from '@/components/Member';
+import React from 'react';
+
+function FadeInSection(props) {
+	const [isVisible, setVisible] = React.useState(false);
+	const domRef = React.useRef();
+	React.useEffect(() => {
+	  const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => entry.isIntersecting && setVisible(true));	
+	  });
+	  observer.observe(domRef.current);
+	  return () => observer.unobserve(domRef.current);
+	}, []);
+	return (
+	  <div
+		className={`${styles['fade-in-section']} ${isVisible ? styles['is-visible'] : ''}`}
+		ref={domRef}>
+		{props.children}
+	  </div>
+	);
+}
+
 
 
 export default function Committees() {
 	let mappedMembers = officerData.map((member) => {
 		return (
-			<Member
-				name={member.name}
-				title={member.title}
-				image={member.image}
-				linkedin={member.linkedin}
-				email={member.email}
-				key={member.name}
-			/>
+			<FadeInSection key={member.name}>
+				<Member
+					name={member.name}
+					title={member.title}
+					image={member.image}
+					linkedin={member.linkedin}
+					email={member.email}
+					key={member.name}
+				/>
+			</FadeInSection>
 		);
 	});
 	return (
 	<>
 		<div className={styles.group__container}>
 			<div className="tw-absolute tw-flex tw-flex-col tw-items-center tw-justify-center tw-w-[100vw] tw-h-full">
-				<div className="tw-justify-center tw-text-white tw-text-[5rem] tw-font-bold">
+				<div className="tw-justify-center tw-text-white tw-text-[5rem] tw-font-bold tw-z-10">
 					<h1 className='tw-text-center tw-text-[6vw]'>2023-2024</h1>
 					<h1 className='tw-text-[6vw]'>IEEE Officers</h1>
 				</div>
@@ -79,3 +97,4 @@ export default function Committees() {
 	// 	</div>
 	// );
 }
+
